7091b200f5c8bb47e8e41665ee7e26c69faf76ca
carlosalegria@me.com <147549641+CarlosATO@users.noreply.github.com>
2025-12-11 14:43:51 -0300

    Fix: Permitir host de Railway en Vite

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5160,      // <--- Aquí forzamos tu puerto 5160
-    strictPort: true, // Falla si el puerto está ocupado (así te das cuenta)
-    host: true       // Permite conexiones externas si es necesario
+    port: 5160,
+    strictPort: true,
+    host: true
+  },
+  preview: {
+    port: process.env.PORT ? Number(process.env.PORT) : 5160,
+    strictPort: true,
+    host: true,
+    // 👇 ESTO ES LO QUE ARREGLA EL ERROR
+    allowedHosts: ['modulologisticasomyl-production.up.railway.app'] 
   }
 })
